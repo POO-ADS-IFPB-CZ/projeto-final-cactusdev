4526daee3cdf11198052ec37dc479f6d257be5bc
Diego Sousa <diego7lts7@gmail.com>
2025-02-05 10:48:16 -0300

    refactor: corrigindo a implementacao do adapter de gerar codigo pela data e um numero random.

--- src/services/adapters/codigo_generator/GenerateWithDateRandom.java
+++ src/services/adapters/codigo_generator/GenerateWithDateRandom.java
@@ -2,13 +2,14 @@ package src.services.adapters.codigo_generator;
 
 import src.model.interfaces.CodigoGenerator;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 public class GenerateWithDateRandom implements CodigoGenerator {
-    private final String saveCode;
-    public GenerateWithDateRandom(String saveCode){
-        this.saveCode = saveCode;
-    }
     @Override
-    public String gerarCodigo() {
-        return this.saveCode;
+    public String gerarCodigo(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyHHmmssSSS") ;
+        return  LocalDateTime.now().format(formatter) + new Random().nextInt(100);
     }
 }
