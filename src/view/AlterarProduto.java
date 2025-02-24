11f3c56903d7d1df9dd3fd6cdc66321f9a93b4a4
Sérgio Henrique <oliveiragadelha.sergio@hotmail.com>
2025-02-24 08:18:10 -0300

    feat: adicionar alteração e remoção de produto e cliente

---
+++ src/view/AlterarProduto.java
@@ -0,0 +1,14 @@
+package src.view;
+
+import javax.swing.*;
+
+public class AlterarProduto {
+    private JTextField nomeField1;
+    private JTextField categoriaField2;
+    private JTextField unidadeMedidaField3;
+    private JTextField quantidadeField4;
+    private JTextField valorField5;
+    private JButton alterarButton;
+    private JButton apagarButton;
+    private JButton cancelarButton;
+}
